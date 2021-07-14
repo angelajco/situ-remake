887009afc0eb6dddaff6d3296feb745b92453220
Juan Luis Pérez Benítez <jut.075@gmail.com>
2021-07-14 18:12:22 -0500

    Se agrega desagregacion en consulta dinamica

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 isProd = process.env.NODE_ENV === 'production';
 const basePath = isProd ? '' : '';
 //Para desarrollo
-//const rutas = isProd ? '' : 'http://172.16.117.11';
-const rutas = isProd ? '' : 'http://localhost:8080';
+const rutas = isProd ? '' : 'http://172.16.117.11';
+// const rutas = isProd ? '' : 'http://localhost:8080';
 //Para QA
 // const rutas = isProd ? '' : 'http://172.16.119.17';
 
