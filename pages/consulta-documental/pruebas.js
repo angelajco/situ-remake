d3eb32d826728e77a2cf45fe8c507bdbf615c331
borozcog1701 <71140302+borozcog1701@users.noreply.github.com>
2021-08-02 11:35:14 -0500

    Actualizacion CD 456

--- pages/consulta-documental/pruebas.js
+++ pages/consulta-documental/pruebas.js
@@ -12,6 +12,10 @@ const cookies = new Cookies()
 export default function consultaMetadatos() {
 
   // Guarda el token que viene en la cookie para verificar que la tenga
+  const tokenCookie = cookies.get('SessionToken')
+  const rolCookie = cookies.get('RolUsuario')
+  const estatusCookie = cookies.get('EstatusUsuario')
+  const usuarioCookie = cookies.get('Usuario')
   const usuarioI = cookies.get('IDU')
 
   //Datos para el modal
